17ea24d7a1c54f5ef80d9022f35178d4f5a1b253
Hongzhang Shan <hongzhang@fb.com>
2020-01-01 17:24:20 -0800

    Add submodule cpuinfo

--- hello.c
+++ hello.c
@@ -1,10 +1,22 @@
 
 #include <stdio.h>
+#include <cpuinfo.h>
 
 int main()
 {
 
-  printf("Hello\n");
+  if (cpuinfo_initialize()) {
+     printf("Hello WITH successfully initialized cpuinfo\n");
+  } else {
+     printf("Hello WITHOUT successfully initialized cpuinfo\n");
+  }
+
+  if (cpuinfo_has_x86_avx512f()) {
+     printf("With avx512 support!\n");
+  } else {
+     printf("Without avx512 support!\n");
+  }
+
   return 0;
 
 }
